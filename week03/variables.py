b6f9bed254c880244267ccb3690b7a9f2f8a81da
gavinra <kjta1015@charter.net>
2022-10-05 15:26:08 -0400

    assignment

---
+++ week03/variables.py
@@ -0,0 +1,25 @@
+x = 10
+y = 20
+print(x, y)
+print("x is", x, "and y is", y)
+print(x, "multiplied by", y, "is", x * y)
+print(x, "multiplied by", y, "is", x * y)
+x = 13.5
+print(x, "divided by", y, "is", x / y)
+x = 10
+y = 20
+result = x + y
+print(x, "plus", y, "is", result)
+a1 = "cat"
+a2 = "dog"
+print("pets:", a1, a2)
+x = 10
+y = 20
+print("%dX%d=%d" % (x, y, x * y))
+x = 10
+y = 20
+print(f"{x} plus {y} is {x + y}")
+f = 5
+g = 12
+print("f is", f, "and g is", g)
+print(f, "plus", g, "multiplied by", f, "is", f * (f + g))
